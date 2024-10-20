180dc1fa8bc758755d4043ae6eac07e63764108b
Stephan Kostin <va.lenta0907@gmail.com>
2024-10-21 01:12:37 +0500

    Update

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
