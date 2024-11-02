33d6bf0f8ddb1dd17bdf30cdefc30d0b49060dd6
Stephan Kostin <va.lenta0907@gmail.com>
2024-11-02 19:32:02 +0500

    Fix

--- src/routes/meseges.js
+++ src/routes/meseges.js
@@ -5,8 +5,8 @@ const { Unauthorized, NotFound } = HttpErrors;
 
 const getNextId = () => _.uniqueId();
 
-export default (app, state) => {
-  app.get('/api/messages', { preValidation: [app.authenticate] }, (req, reply) => {
+export default (server, state) => {
+  server.get('/api/messages', { preValidation: [server.authenticate] }, (req, reply) => {
     const user = state.users.find(({ id }) => id === req.user.userId);
 
     if (!user) {
@@ -15,11 +15,11 @@ export default (app, state) => {
     }
 
     reply
-      .header('Content-Type', 'application/json; charset=utf-8')
+      .header('Content-Type', 'serverlication/json; charset=utf-8')
       .send(state.messages);
   });
 
-  app.post('/api/messages', { preValidation: [app.authenticate] }, async (req, reply) => {
+  server.post('/api/messages', { preValidation: [server.authenticate] }, async (req, reply) => {
     const message = req.body;
     const messageWithId = {
       ...message,
@@ -27,14 +27,14 @@ export default (app, state) => {
       id: getNextId(),
     };
     state.messages.push(messageWithId);
-    app.io.emit('newMessage', messageWithId);
+    server.io.emit('newMessage', messageWithId);
 
     reply
-      .header('Content-Type', 'application/json; charset=utf-8')
+      .header('Content-Type', 'serverlication/json; charset=utf-8')
       .send(messageWithId);
   });
 
-  app.patch('/api/messages/:messageId', { preValidation: [app.authenticate] }, async (req, reply) => {
+  server.patch('/api/messages/:messageId', { preValidation: [server.authenticate] }, async (req, reply) => {
     const { messageId } = req.params;
     const { body } = req.body;
     const message = state.messages.find((c) => c.id === messageId);
@@ -44,20 +44,20 @@ export default (app, state) => {
     }
     message.body = body;
 
-    app.io.emit('renameMessage', message);
+    server.io.emit('renameMessage', message);
     reply
-      .header('Content-Type', 'application/json; charset=utf-8')
+      .header('Content-Type', 'serverlication/json; charset=utf-8')
       .send(message);
   });
 
-  app.delete('/api/messages/:messageId', { preValidation: [app.authenticate] }, async (req, reply) => {
+  server.delete('/api/messages/:messageId', { preValidation: [server.authenticate] }, async (req, reply) => {
     const { messageId } = req.params;
     state.messages = state.messages.filter((m) => m.id !== messageId);
     const data = { id: messageId };
 
-    app.io.emit('removeMessage', data);
+    server.io.emit('removeMessage', data);
     reply
-      .header('Content-Type', 'application/json; charset=utf-8')
+      .header('Content-Type', 'serverlication/json; charset=utf-8')
       .send(data);
   });
 };
