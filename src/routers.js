76905e66d45d5555c777af9ba77d4676d5219a6a
Stephan Kostin <va.lenta0907@gmail.com>
2024-11-07 23:12:57 +0500

    Added verification for socket io

--- src/routers.js
+++ src/routers.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import HttpErrors from 'http-errors';
 
 import initUsersRoutes from './routes/users.js';
 import initMessagesRoutes from './routes/meseges.js';
 import initChannelsRoutes from './routes/chanels.js';
+
+const { Unauthorized } = HttpErrors;
 
 const getId = () => _.uniqueId();
 
@@ -23,21 +26,35 @@ const buildStates = (defaultStates) => {
     users: [],
   };
 
-  if (defaultStates.messages) {
-    state.messages.push(...defaultStates.messages);
-  }
-  if (defaultStates.channels) {
-    state.channels.push(...defaultStates.channels);
-  }
-  if (defaultStates.currentChannelId) {
-    state.currentChannelId = defaultStates.currentChannelId;
-  }
-  if (defaultStates.users) {
-    state.users.push(...defaultStates.users);
-  }
+  if (defaultStates.messages) state.messages.push(...defaultStates.messages);
+  if (defaultStates.channels) state.channels.push(...defaultStates.channels);
+  if (defaultStates.currentChannelId) state.currentChannelId = defaultStates.currentChannelId;
+  if (defaultStates.users) state.users.push(...defaultStates.users);
 
   return state;
 };
+
+const setSocketAuth = (server, state) => {
+  server.io.use((socket, next) => {
+    const isHandshake = socket.handshake.query.sid === undefined;
+    if (!isHandshake) return next();
+
+    const header = socket.handshake.headers["authorization"];
+    if (!header) return next(new Error('401'));
+    if (!header.startsWith("Bearer ")) return next(new Error('401'));
+
+    const token = header.substring(7);
+    if (token === 'null') return next(new Error('401'));
+
+    server.jwt.verify(token, 'supersecret', (err, decoded) => {
+      const user = state.users.find(({ id }) => id === decoded.userId);
+      if (err || !user) return next(new Error('401'));
+
+      socket.user = decoded.data;
+      next();
+    });
+  });
+}
 
 export default (server, defualtStates = {}) => {
   const state = buildStates(defualtStates);
@@ -46,16 +63,7 @@ export default (server, defualtStates = {}) => {
     console.log({ 'socket.id': socket.id });
   });
 
-  // server.io.use((socket, next) => {
-  //   try {
-  //     server.authenticate(socket.request);
-  //     console.log(12);
-  //     next();
-  //   } catch(err) {
-  //     console.log(123);
-  //     next()
-  //   }
-  // });
+  setSocketAuth(server, state);
 
   initChannelsRoutes(server, state);
   initMessagesRoutes(server, state);
